b71feeca64f16f63fc86e5dbc06695ce8fb103ab
William Xu <yihangxu001@hotmail.com>
2021-07-22 11:15:21 -0700

    Write functions

--- tests/testthat/test-dose.R
+++ tests/testthat/test-dose.R
@@ -1,1 +1,51 @@
 library(testthat)
+
+test_output <- function() {
+  test_that('each condition gives the expected output', {
+    expect_equal(
+      dose(
+        mmrc=1,
+        fev=55,
+        smoking_status=0,
+        exacerbation=0
+      )
+      ,
+      0
+    )
+
+    expect_equal(
+      dose(
+        mmrc=2,
+        fev=33,
+        smoking_status=1,
+        exacerbation=2
+      )
+      ,
+      4
+    )
+
+    expect_equal(
+      dose(
+        mmrc=3,
+        fev=29,
+        smoking_status=1,
+        exacerbation=4
+      )
+      ,
+      7
+    )
+
+    expect_equal(
+      dose(
+        mmrc=4,
+        fev=29,
+        smoking_status=1,
+        exacerbation=4
+      )
+      ,
+      8
+    )
+  })
+}
+
+test_output()
