bb80a2012b679666d4098f11634702ce79c6410b
William Xu <67845982+williamxu7@users.noreply.github.com>
2021-07-28 16:38:03 -0700

    Update test-dose.R

--- tests/testthat/test-dose.R
+++ tests/testthat/test-dose.R
@@ -8,7 +8,7 @@ test_output <- function() {
         fev=55,
         smoking_status=0,
         exacerbation=0
-      )
+      )$dose_index
       ,
       0
     )
@@ -19,7 +19,7 @@ test_output <- function() {
         fev=33,
         smoking_status=1,
         exacerbation=2
-      )
+      )$dose_index
       ,
       4
     )
@@ -30,7 +30,7 @@ test_output <- function() {
         fev=29,
         smoking_status=1,
         exacerbation=4
-      )
+      )$dose_index
       ,
       7
     )
@@ -41,7 +41,7 @@ test_output <- function() {
         fev=29,
         smoking_status=1,
         exacerbation=4
-      )
+      )$dose_index
       ,
       8
     )
